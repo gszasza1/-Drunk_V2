ecb323ae23366a321ed093ead27e5844c2596846
gszasza1 <gszasza1@gmail.com>
2020-09-14 00:42:26 +0200

    feat: added material

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import 'vue-material/dist/theme/default.css';
+import 'vue-material/dist/vue-material.min.css';
+
+import { createApp } from 'vue';
+
+import App from './App.vue';
 
 createApp(App).mount("#app");
