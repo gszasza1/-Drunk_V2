7756708f4df7003ef96342102d7706be36e013ce
gszasza1 <gszasza1@gmail.com>
2020-09-27 13:20:15 +0200

    feat: config material

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
+import './app.scss';
 import './registerServiceWorker';
-import 'vue-material/dist/theme/default.css';
+import 'vue-material/dist/theme/default-dark.css';
 import 'vue-material/dist/vue-material.min.css';
 
 import Vue from 'vue';
@@ -11,6 +12,70 @@ import store from './store';
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
+
+const AltVue = Vue as any;
+AltVue.material = {
+  ...AltVue.material,
+  locale: {
+    ...AltVue.material.locale,
+    dateFormat: "yyyy. MM. dd.",
+    firstDayOfAWeek: 1,
+    days: [
+      "Vasárnap",
+      "Hétfő",
+      "Kedd",
+      "Szerda",
+      "Csütörtök",
+      "Péntek",
+      "Szombat",
+    ],
+    shortDays: ["Vasá", "Hétf", "Kedd", "Szer", "Csüt", "Pén", "Szomb"],
+    shorterDays: ["V", "H", "K", "Sz", "Cs", "P", "Sz"],
+    months: [
+      "Január",
+      "Február",
+      "Március",
+      "Április",
+      "Május",
+      "Június",
+      "Július",
+      "Agusztus",
+      "Szeptember",
+      "Október",
+      "November",
+      "December",
+    ],
+    shortMonths: [
+      "Jan",
+      "Feb",
+      "Már",
+      "Ápr",
+      "Máj",
+      "Jún",
+      "Júl",
+      "Aug",
+      "Szept",
+      "Okt",
+      "Nov",
+      "Dec",
+    ],
+    shorterMonths: [
+      "J",
+      "F",
+      "M",
+      "Á",
+      "M",
+      "Jú",
+      "Jú",
+      "Á",
+      "Sze",
+      "O",
+      "N",
+      "D",
+    ],
+  },
+};
+
 new Vue({
   router,
   store,
