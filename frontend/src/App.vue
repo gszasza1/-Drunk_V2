24f044dd2644927d6eb182b84efd07b65710a76c
gszasza1 <gszasza1@gmail.com>
2020-09-28 22:22:30 +0200

    feat: added header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,28 @@
 <template>
     <div id="app">
-        <router-view />
+        <md-toolbar md-elevation="0">
+            <md-button to="/registration" class="md-primary"
+                >Regisztráció</md-button
+            >
+            <md-button to="/login" class="md-primary">Bejelentkezés</md-button>
+        </md-toolbar>
+        <div class="content">
+            <router-view />
+        </div>
     </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    width: 100vw;
-    height: 100vh;
+    min-width: 100vw;
+    min-height: 100vh;
     display: flex;
-    * > {
-        width: 100%;
-        height: 100%;
-    }
-}
-
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
+    flex-direction: column;
+    > .content {
+        flex: 1;
+        display: flex;
+        > * {
+            flex: 1;
         }
     }
 }
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="home">
-        <md-app>
-            <img alt="Vue logo" src="../assets/logo.png" />
-            <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-        </md-app>
+        Drunk
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-    components: {
-        HelloWorld
-    }
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+@import '../variables.scss';
+.home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 70px;
+    text-transform: uppercase;
+    color: $secondary;
+    background: $primary;
+    width: 100%;
+    font-weight: 800;
+}
+</style>
